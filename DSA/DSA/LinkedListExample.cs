4d8d4c07a6ea34bd84c5866dd63c08304c2b0749
Felipe Lira <filipi6219@gmail.com>
2024-07-27 16:31:42 -0300

    feat: add exemplo de linked list

---
+++ DSA/DSA/LinkedListExample.cs
@@ -0,0 +1,31 @@
+﻿using DataStructures;
+
+namespace DSA
+{
+    internal static class LinkedListExample
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedList list = new MyLinkedList();
+            list.PutAtEnd(1);
+            list.PutAtEnd(2);
+            list.PutAtEnd(3);
+            list.PutAtEnd(4);
+            list.Print();
+
+            list.PutAtBeginning(0);
+            list.PutAtBeginning(-1);
+            list.PutAtBeginning(-2);
+            Console.WriteLine("linked list after inserting 0, -1, and -2:");
+            list.Print();
+
+            list.DeleteNode(3);
+            Console.WriteLine("linked list after deleting position 3:");
+            list.Print();
+
+            list.DeleteNode(0);
+            Console.WriteLine("linked list after deleting position 0:");
+            list.Print();
+        }
+    }
+}
--- DSA/DSA/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DSA
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
