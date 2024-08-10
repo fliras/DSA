f6ba66e5011e41eedb06e236374460b56d9f0a79
Felipe Lira <filipi6219@gmail.com>
2024-08-10 09:05:14 -0300

    feat: melhorar exemplo de bubble sort

--- DSA/DSA/BubbleSortExample.cs
+++ DSA/DSA/BubbleSortExample.cs
@@ -6,13 +6,30 @@ namespace DSA
     {
         static void Main(string[] args)
         {
-            int[] array = { 5, 2, -1, 4, 3, 98, 100, 0, 666, -666, 319, 475, 89, 69, 666, -666 };
-            Console.WriteLine(String.Join(",", array));
+            int[] array1 = new int[] { 5, -1, 4, 3, 92, 78, 78 };
+            PrintArray(array1);
+            BubbleSort.Handle(array1);
+            PrintArray(array1);
+            Console.WriteLine();
 
-            BubbleSort.Handle(array);
-            Console.WriteLine(String.Join(",", array));
+            int[] array2 = new int[] { 5, 4, 3, 2, 1, 0, -1, -2 };
+            PrintArray(array2);
+            BubbleSort.Handle(array2);
+            PrintArray(array2);
+            Console.WriteLine();
+
+            int[] array3 = new int[] { 0, 0, 0, -34, 34, 3, 2 };
+            PrintArray(array3);
+            BubbleSort.Handle(array3);
+            PrintArray(array3);
+            Console.WriteLine();
 
             Console.ReadKey();
+        }
+
+        static void PrintArray(int[] array)
+        {
+            Console.WriteLine(String.Join(",", array));
         }
     }
 }
