2222fd3b34907f28988bdfea20c6a6da47ff527c
Felipe Lira <filipi6219@gmail.com>
2024-08-10 09:45:34 -0300

    feat: melhorar exemplo de array based queue

--- DSA/DSA/ArrayBasedQueueExample.cs
+++ DSA/DSA/ArrayBasedQueueExample.cs
@@ -7,27 +7,33 @@ namespace DSA
         static void Mainha(string[] args)
         {
             CustomArrayBasedQueue<int> queue = new CustomArrayBasedQueue<int>(5);
-            Console.WriteLine($"is empty: {queue.IsEmpty()}");
 
-            queue.Enqueue(1);
-            queue.Enqueue(2);
-            queue.Enqueue(3);
-            queue.Enqueue(4);
-            Console.WriteLine($"is empty: {queue.IsEmpty()}");
-            Console.WriteLine($"is full: {queue.IsFull()}");
+            Console.WriteLine($"queue is empty: {queue.IsEmpty()}"); // true
+            Console.WriteLine($"queue is full: {queue.IsFull()}"); // false
+            queue.Enqueue(13);
+            Console.WriteLine($"queue is empty: {queue.IsEmpty()}"); // false
 
-            queue.Enqueue(5);
-            Console.WriteLine($"is full: {queue.IsFull()}");
-            queue.Enqueue(6);
+            queue.Enqueue(12);
+            queue.Enqueue(11);
+            queue.Enqueue(10);
+            queue.Enqueue(9);
+            Console.WriteLine($"queue is full: {queue.IsFull()}"); // true
+            queue.Print();
 
-            Console.WriteLine();
+            Console.ReadKey();
 
-            queue.DeQueue();
-            queue.DeQueue();
-            queue.DeQueue();
-            queue.DeQueue();
-            queue.DeQueue();
-            Console.WriteLine($"is empty: {queue.IsEmpty()}");
+            queue.Dequeue();
+            queue.Dequeue();
+            Console.WriteLine($"queue is full: {queue.IsFull()}"); // false
+            queue.Print();
+
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Print();
+            Console.WriteLine($"queue is full: {queue.IsFull()}"); // false
+            Console.WriteLine($"queue is full: {queue.IsEmpty()}"); // true
+
             Console.ReadKey();
         }
     }
--- DSA/DataStructures/CustomArrayBasedQueue.cs
+++ DSA/DataStructures/CustomArrayBasedQueue.cs
@@ -2,29 +2,9 @@
 {
     public class CustomArrayBasedQueue<T>(int length)
     {
-        private readonly T[] _array = new T[length];
         private int _front = -1;
         private int _rear = -1;
-
-        public void Enqueue(T value)
-        {
-            if (IsFull()) return;
-            if (_front == -1) _front++;
-            _array[++_rear] = value;
-        }
-
-        public void DeQueue()
-        {
-            if (IsEmpty()) return;
-            if (_front == _rear) _front = _rear = -1;
-            else _front++;
-        }
-
-        public T? Peek()
-        {
-            if (IsEmpty()) return default;
-            return _array[_front];
-        }
+        private readonly T[] _array = new T[length];
 
         public bool IsEmpty()
         {
@@ -34,6 +14,43 @@
         public bool IsFull()
         {
             return _rear == _array.Length - 1;
+        }
+
+        public void Enqueue(T value)
+        {
+            if (IsFull()) throw new ArithmeticException("Queue is full");
+            if (IsEmpty()) _front++;
+            _array[++_rear] = value;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty()) throw new ArithmeticException("Queue is empty");
+            T value = Peek();
+            if (_front == _rear) _front = _rear = -1;
+            else _front++;
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty()) throw new ArithmeticException("Queue is empty");
+            return _array[_front];
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("[]");
+            }
+            else
+            {
+                Console.Write("[");
+                for (int i = _front; i <= _rear; i++)
+                    Console.Write($"{_array[i]},");
+                Console.WriteLine("]");
+            }
         }
     }
 }
