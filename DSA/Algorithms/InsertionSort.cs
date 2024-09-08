37a64894c4432e34589ba211916d8253c8635d83
Felipe Lira <filipi6219@gmail.com>
2024-09-08 19:24:54 -0300

    feat: add exemplo de insertion sort

---
+++ DSA/Algorithms/InsertionSort.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms
+{
+    public static class InsertionSort
+    {
+        public static void Handle(int[] array)
+        {
+            if (array.Length <= 1) return;
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = key;
+            }
+        }
+    }
+}
---
+++ DSA/DSA/InsertionSortExample.cs
@@ -0,0 +1,40 @@
+﻿using Algorithms;
+
+namespace DSA
+{
+    internal static class InsertionSortExample
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 5, -1, 4, 3, 92, 78, 78 };
+            int[] array2 = { 5, 4, 3, 2, 1, 0, -1, -2 };
+            int[] array3 = { 0, 0, 0, -34, 34, 3, 2 };
+
+            Console.WriteLine("array 1 desordenado:");
+            PrintarArray(array1);
+            InsertionSort.Handle(array1);
+            Console.WriteLine("array 1: ordenado: ");
+            PrintarArray(array1);
+
+
+            Console.WriteLine("array 2 desordenado:");
+            PrintarArray(array2);
+            InsertionSort.Handle(array2);
+            Console.WriteLine("array 2: ordenado: ");
+            PrintarArray(array2);
+
+            Console.WriteLine("array 3 desordenado:");
+            PrintarArray(array3);
+            InsertionSort.Handle(array3);
+            Console.WriteLine("array 3: ordenado: ");
+            PrintarArray(array3);
+
+            Console.ReadKey();
+        }
+
+        static void PrintarArray(int[] array)
+        {
+            Console.WriteLine(String.Join(",", array));
+        }
+    }
+}
