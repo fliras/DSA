5d52b5d6f87eb6d3dcc4dd0ecc85d35d279ae6e7
Felipe Lira <filipi6219@gmail.com>
2024-09-17 05:26:01 -0300

    feat: modificar exemplo de bubbleSort

--- DSA/Algorithms/BubbleSort.cs
+++ DSA/Algorithms/BubbleSort.cs
@@ -4,15 +4,15 @@
     {
         public static void Handle(int[] array)
         {
-            for (int i = 1; i < array.Length; i++)
+            for (int i = 0; i < array.Length - 1; i++)
             {
                 bool swapped = false;
-                for (int j = 0; j < array.Length - i; j++)
+                for (int j = 0; j < array.Length - i - 1; j++)
                 {
                     if (array[j] > array[j + 1])
                     {
-                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
                         swapped = true;
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
                     }
                 }
                 if (!swapped) break;
