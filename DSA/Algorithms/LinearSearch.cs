373a99458f7491ed435758d3c1518fda30bc56fe
Felipe Lira <filipi6219@gmail.com>
2024-09-22 19:32:09 -0300

    feat: add exemplo de linear search

---
+++ DSA/Algorithms/LinearSearch.cs
@@ -0,0 +1,15 @@
+﻿namespace Algorithms
+{
+    public static class LinearSearch
+    {
+        public static int Handle(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == element)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ DSA/DSA/LinearSearchExample.cs
@@ -0,0 +1,21 @@
+﻿using Algorithms;
+
+namespace DSA
+{
+    internal static class LinearSearch
+    {
+        static void Mainha(string[] args)
+        {
+            int[] array = { -2, -1, 0, 1, 2, 3, 4 };
+            int elemento1 = BinarySearch.Handle(array, 1);
+            int elemento2 = BinarySearch.Handle(array, 2);
+            int elemento5 = BinarySearch.Handle(array, 5);
+
+            Console.WriteLine("1: " + elemento1); // 3
+            Console.WriteLine("2: " + elemento2); // 4
+            Console.WriteLine("5: " + elemento5); // -1
+
+            Console.ReadKey();
+        }
+    }
+}
