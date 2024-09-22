a4ecdca480bdd5a9ec041b3501d84a9029e875db
Felipe Lira <filipi6219@gmail.com>
2024-09-22 19:10:55 -0300

    feat: add exemplo de binary search

---
+++ DSA/Algorithms/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class BinarySearch
+    {
+        public static int Handle(int[] array, int element)
+        {
+            int start = 0;
+            int end = array.Length - 1;
+            while (start <= end)
+            {
+                int middle = (start + end) / 2;
+                if (array[middle] == element)
+                    return middle;
+                else if (array[middle] < element)
+                    start = middle + 1;
+                else
+                    end = middle - 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ DSA/DSA/BinarySearchExample.cs
@@ -0,0 +1,26 @@
+﻿using Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    internal static class BinarySearchExample
+    {
+        static void Mainha(string[] args)
+        {
+            int[] array = { -2, -1, 0, 1, 2, 3, 4 };
+            int elemento1 = BinarySearch.Handle(array, 1);
+            int elemento2 = BinarySearch.Handle(array, 2);
+            int elemento5 = BinarySearch.Handle(array, 5);
+
+            Console.WriteLine("1: " + elemento1); // 3
+            Console.WriteLine("2: " + elemento2); // 4
+            Console.WriteLine("5: " + elemento5); // -1
+
+            Console.ReadKey();
+        }
+    }
+}
